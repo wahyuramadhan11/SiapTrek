b445ad6bb333fd6127980db5d71834e44cb13a88
wahyuramadhan11 <rezkantal1@gmail.com>
2026-06-09 23:04:08 +0700

    Update login.php

--- login.php
+++ login.php
@@ -33,6 +33,7 @@ if(isset($_POST['login'])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login - SiapTrek</title>
+    <link rel="icon" href="img/logo.jpg">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Syne:wght@400;600;700;800&family=DM+Sans:wght@300;400;500&display=swap" rel="stylesheet">
@@ -442,7 +443,8 @@ if(isset($_POST['login'])) {
     <!-- ===== LEFT PANEL ===== -->
     <div class="left-panel">
         <a href="index.php" class="brand">
-            <div class="brand-icon"><i class="bi bi-campground"></i></div>
+            <img src="img/logo.jpg" alt="SiapTrek Logo" height="30" class="d-inline-block align-text-top me-2">
+    
             <span class="brand-name">SiapTrek</span>
         </a>
 
